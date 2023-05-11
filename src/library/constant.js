b21a53faf7095559506417c3fe49eb6f4bd8a11d
Silvanus930 <silvanus930@outlook.com>
2023-05-11 22:35:07 +0900

    update: be url

--- src/library/constant.js
+++ src/library/constant.js
@@ -1,3 +1,4 @@
 const API_PORT = process.env.REACT_APP_API_PORT;
+// export const API_URL = `${process.env.REACT_APP_API_URL}${API_PORT ? (":" + API_PORT) : ""}/`;
 
-export const API_URL = `${process.env.REACT_APP_API_URL}${API_PORT ? (":" + API_PORT) : ""}/`;
+export const API_URL = `https://03b3-103-6-219-221.ngrok-free.app/`;
