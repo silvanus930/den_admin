a6b2cfd149d4191faad803c0a6aaafc8101558b9
Silvanus930 <silvanus930@outlook.com>
2023-05-16 20:34:42 +0900

    update: backend api url path

--- src/library/constant.js
+++ src/library/constant.js
@@ -1,3 +1,3 @@
 const API_PORT = process.env.REACT_APP_API_PORT;
-export const API_URL = `${process.env.REACT_APP_API_URL}${API_PORT ? (":" + API_PORT) : ""}/api/`;
-// export const API_URL = `http://13.50.98.6/api/`;
+// export const API_URL = `${process.env.REACT_APP_API_URL}${API_PORT ? (":" + API_PORT) : ""}/api/`;
+export const API_URL = `http://13.50.98.6/api/`;
