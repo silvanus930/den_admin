6912f695e06f052caf5670b54cbb106ba60eecde
jef_dev <jefwil_sun@gmail.com>
2023-06-27 20:48:40 +0200

    update: site url

--- src/library/constant.js
+++ src/library/constant.js
@@ -3,5 +3,5 @@ export const UPLOAD_PUBLIC_KEY = "public_kW15bKjAVCqFHnguNbe46VeVPXjg";
 // export const API_URL = `http://localhost:3001/api/`;
 // export const BOT_URL = `http://localhost:3000/`;
 
-export const API_URL = `http://13.50.98.6/api/`;
-export const BOT_URL = `http://13.50.98.6/`;
+export const API_URL = `http://app2.denbot.co.uk/api/`;
+export const BOT_URL = `http://app2.denbot.co.uk/`;
